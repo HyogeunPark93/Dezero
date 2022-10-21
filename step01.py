ea51eb8af2008a164566b65dba7387c1eb03e9e4
HyoGeun <nilson2043@gmail.com>
2022-10-21 10:59:27 +0900

    second commit

--- step01.py
+++ step01.py
@@ -1,19 +1,62 @@
+from cgitb import grey
 import numpy as np
 
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
         
 class Function:
     def __call__(self, input):
         x = input.data
-        y = x ** 2
+        y = self.forward(x)
         output = Variable(y)
+        self.input = input
         return output
+    
+    def forward(self, x): # 순전파
+        raise NotImplementedError()
+    
+    def backward(self,x):
+        raise NotImplementedError()
+    
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
         
-x =  Variable(np.array(10.0))
-f = Function()
-y = f(x)
+  
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+    
+def numerical_diff(f, x, eps = 1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data / (2 * eps))
+    
+A = Square()
+B = Exp()
+C = Square()
 
-print(type(y))
-print(y.data)
+x = Variable(np.array(.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
+print(x.grad)
